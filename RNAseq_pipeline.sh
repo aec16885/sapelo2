62b8bf465d00757db9c8ad084352048ce397fc72
Audrey <audrey.calvird@uga.edu>
2019-11-15 10:29:40 -0500

    things

--- RNAseq_pipeline.sh
+++ RNAseq_pipeline.sh
@@ -14,11 +14,11 @@ wget "ftp://ftp.ensembl.org/pub/release-98/fasta/danio_rerio/cds/Danio_rerio.GRC
 module load kallisto/0.43.1-foss-2016b
 kallisto index -i transcripts.idx Danio_rerio.GRCz11.cds.all.fa.gz
 
-kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_1.output -b 100 [nsd1mzko_1 1] [nsd1mzko_1 2]
-kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_2.output -b 100 [nsd1mzko_2 1] [nsd1mzko_1 2]
-kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_3.output -b 100 [nsd1mzko_3 1] [nsd1mzko_3 2]
+kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_1.output -b 100 Nsd1a_bdouble-1_IGO_07846_22_S31_R1_001.fastq.gz Nsd1a_bdouble-1_IGO_07846_22_S31_R2_001.fastq.gz
+kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_2.output -b 100 Nsd1a_bdouble-2_IGO_07846_23_S32_R1_001.fastq.gz Nsd1a_bdouble-2_IGO_07846_23_S32_R2_001.fastq.gz
+kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_3.output -b 100 Nsd1a_bdouble-3_IGO_07846_24_S33_R1_001.fastq.gz Nsd1a_bdouble-3_IGO_07846_24_S33_R2_001.fastq.gz
 
-kallisto quant -i transcripts.idx -o kallisto/ab_1.output -b 100 [ab_1 1] [ab_1 2]
+kallisto quant -i transcripts.idx -o kallisto/ab_1.output -b 100 Wt5dpf-1_IGO_07846_19_S28_R1_001.fastq.gz Wt5dpf-1_IGO_07846_19_S28_R2_001.fastq.gz
 kallisto quant -i transcripts.idx -o kallisto/ab_2.output -b 100 [ab_2 1] [ab_2 2]
 kallisto quant -i transcripts.idx -o kallisto/ab_3.output -b 100 [ab_3 1] [ab_3 2]
 
--- volcanoplot.r
+++ volcanoplot.r
@@ -17,9 +17,19 @@ samples_to_conditions <- data.frame(sample,condition)
 samples_to_conditions <- dplyr::mutate(samples_to_conditions, path = kallisto_dirs)
 print(samples_to_conditions)
 
+
 #run sleuth on the data
 sleuth_object <- sleuth_prep(samples_to_conditions, extra_bootstrap_summary = TRUE) # read data into sleuth_object
 sleuth_object <- sleuth_fit(sleuth_object, ~condition, 'full') # estimate parameters for the full linear model that includes the conditions as factors
 sleuth_object <- sleuth_fit(sleuth_object, ~1, 'reduced') # estimate parameters for the reduced linear model that assumes equal transcript abundances in both conditions
 sleuth_object <- sleuth_lrt(sleuth_object, 'reduced', 'full') # perform likelihood ratio test to identify transcripts whose fit is significantly better under full model relative to reduced model
 models(sleuth_object)
+
+sleuth_table <- sleuth_results(sleuth_object, 'reduced:full', 'lrt', show_all = FALSE)
+sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05)
+
+#make volcano plot
+plot_volcano(sleuth_object, test, test_type="wt", which_model = "full", sig_level = 0.05, point_alpha = 0.2, sig_color = "red", highlight= NULL)
+
+#make heat map
+plot_sample_heatmap(sleuth_object, use_filtered= TRUE, color_high='red', color_low='blue', x_axis_angle = 90, 
