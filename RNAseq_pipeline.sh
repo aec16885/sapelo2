12ba69f122433f20c714a764c78c0dd43a5bfa32
Audrey <audrey.calvird@uga.edu>
2019-12-03 09:46:26 -0500

    messed up git hub

--- RNAseq_pipeline.sh
+++ RNAseq_pipeline.sh
@@ -1,6 +1,6 @@
 #PBS -S /bin/bash
 #PBS -q batch
-#PBS -N testjob
+#PBS -N kallisto
 #PBS -l nodes=1:ppn=48:AMD
 #PBS -l walltime=48:00:00
 #PBS -l mem=10gb
@@ -8,19 +8,20 @@
 #PBS -m abe
 
 cd $PBS_O_WORKDIR
+cd /scratch/aec16885/nsd1_RNAseq/bowtie
 
 wget "ftp://ftp.ensembl.org/pub/release-98/fasta/danio_rerio/cds/Danio_rerio.GRCz11.cds.all.fa.gz"
 
 module load kallisto/0.43.1-foss-2016b
 kallisto index -i transcripts.idx Danio_rerio.GRCz11.cds.all.fa.gz
 
-kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_1.output -b 100 Nsd1a_bdouble-1_IGO_07846_22_S31_R1_001.fastq.gz Nsd1a_bdouble-1_IGO_07846_22_S31_R2_001.fastq.gz
-kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_2.output -b 100 Nsd1a_bdouble-2_IGO_07846_23_S32_R1_001.fastq.gz Nsd1a_bdouble-2_IGO_07846_23_S32_R2_001.fastq.gz
-kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_3.output -b 100 Nsd1a_bdouble-3_IGO_07846_24_S33_R1_001.fastq.gz Nsd1a_bdouble-3_IGO_07846_24_S33_R2_001.fastq.gz
+kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_1.output -b 100 n1_R1.trim.gz n1_R2.trim.gz
+kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_2.output -b 100 n2_R1.trim.gz n2_R2.trim.gz
+kallisto quant -i transcripts.idx -o kallisto/nsd1mzko_3.output -b 100 n3_R1.trim.gz n3_R2.trim.gz
 
-kallisto quant -i transcripts.idx -o kallisto/ab_1.output -b 100 Wt5dpf-1_IGO_07846_19_S28_R1_001.fastq.gz Wt5dpf-1_IGO_07846_19_S28_R2_001.fastq.gz
-kallisto quant -i transcripts.idx -o kallisto/ab_2.output -b 100 [ab_2 1] [ab_2 2]
-kallisto quant -i transcripts.idx -o kallisto/ab_3.output -b 100 [ab_3 1] [ab_3 2]
+kallisto quant -i transcripts.idx -o kallisto/ab_1.output -b 100 wt1_R1.trim.gz wt1_R2.trim.gz
+kallisto quant -i transcripts.idx -o kallisto/ab_2.output -b 100 wt2_R1.trim.gz wt2_R2.trim.gz
+kallisto quant -i transcripts.idx -o kallisto/ab_3.output -b 100 wt3_R1.trim.gz wt3_R2.trim.gz
 
 module load R/3.4.4-foss-2016b-X11-20160819-GACRC
 R --no-save < /home/aec16885/nsd1/5dpf_RNAseq/volcanoplot.r
---
+++ hisat2.sh
@@ -0,0 +1,74 @@
+#PBS -S /bin/bash
+#PBS -q batch
+#PBS -N cufflinks
+#PBS -l nodes=1:ppn=48:AMD
+#PBS -l walltime=72:00:00
+#PBS -l mem=10gb
+#PBS -M audrey.calvird@uga.edu
+#PBS -m abe
+
+cd $PBS_O_WORKDIR
+cd /scratch/aec16885/nsd1_RNAseq/bowtie/hisat2
+
+module load HISAT2/2.1.0-foss-2016b
+
+hisat2-build GCF_000002035.6_GRCz11_genomic.fna GRCz11_index
+
+hisat2 -f -x GRCz11_index -1 ../n1_R1.trim.gz -2 ../n1_R2.trim.gz -S n1_alignment.sam
+hisat2 -f -x GRCz11_index -1 ../n2_R1.trim.gz -2 ../n2_R2.trim.gz -S n2_alignment.sam
+hisat2 -f -x GRCz11_index -1 ../n3_R1.trim.gz -2 ../n3_R2.trim.gz -S n3_alignment.sam
+
+hisat2 -f -x GRCz11_index -1 ../wt1_R1.trim.gz -2 ../wt1_R2.trim.gz -S wt1_alignment.sam
+hisat2 -f -x GRCz11_index -1 ../wt2_R1.trim.gz -2 ../wt2_R2.trim.gz -S wt2_alignment.sam
+hisat2 -f -x GRCz11_index -1 ../wt3_R1.trim.gz -2 ../wt3_R2.trim.gz -S wt2_alignment.sam
+
+
+module load SAMtools/1.6-foss-2016b
+
+samtools view -b n1_alignment.sam > n1_alignment.bam
+samtools view -b n2_alignment.sam > n2_alignment.bam
+samtools view -b n3_alignment.sam > n3_alignment.bam
+
+samtools view -b wt1_alignment.sam > wt1_alignment.bam
+samtools view -b wt2_alignment.sam > wt2_alignment.bam
+samtools view -b wt3_alignment.sam > wt3_alignment.bam
+
+samtools sort -O SAM n1_alignment.bam > n1_alignment.sorted.sam
+samtools sort -O SAM n2_alignment.bam > n2_alignment.sorted.sam
+samtools sort -O SAM n3_alignment.bam > n3_alignment.sorted.sam
+
+samtools sort -O SAM wt1_alignment.bam > wt1_alignment.sorted.sam
+samtools sort -O SAM wt2_alignment.bam > wt2_alignment.sorted.sam
+samtools sort -O SAM wt3_alignment.bam > wt3_alignment.sorted.sam
+
+samtools view -b n1_alignment.sorted.sam > n1_alignment.sorted.bam
+samtools view -b n2_alignment.sorted.sam > n2_alignment.sorted.bam
+samtools view -b n3_alignment.sorted.sam > n3_alignment.sorted.bam
+
+samtools view -b wt1_alignment.sorted.sam > wt1_alignment.sorted.bam
+samtools view -b wt2_alignment.sorted.sam > wt2_alignment.sorted.bam
+samtools view -b wt3_alignment.sorted.sam > wt3_alignment.sorted.bam
+
+module load SAMtools/0.1.19-foss-2016b
+module load Cufflinks/2.2.1-foss-2016b
+
+cufflinks -o cufflinks_output_n1/ -g Danio_rerio.GRCz11.98.gtf n1_alignment.sorted.bam
+cufflinks -o cufflinks_output_n2/ -g Danio_rerio.GRCz11.98.gtf n2_alignment.sorted.bam
+cufflinks -o cufflinks_output_n3/ -g Danio_rerio.GRCz11.98.gtf n3_alignment.sorted.bam
+
+cufflinks -o cufflinks_output_wt1/ -g Danio_rerio.GRCz11.98.gtf wt1_alignment.sorted.bam
+cufflinks -o cufflinks_output_wt2/ -g Danio_rerio.GRCz11.98.gtf wt2_alignment.sorted.bam
+cufflinks -o cufflinks_output_wt3/ -g Danio_rerio.GRCz11.98.gtf wt3_alignment.sorted.bam
+
+cuffcompare -o cuffcompare_all_out Danio_rerio.GRCz11.98.gtf cufflinks_output_n1/transcripts.gtf cufflinks_output_n2/transcripts.gtf cufflinks_output_n3/transcripts.gtf cufflinks_output_wt1/transcripts.gtf cufflinks_output_wt2/transcripts.gtf cufflinks_output_wt3/transcripts.gtf
+cuffcompare -o cuffcompare_nsd1_out Danio_rerio.GRCz11.98.gtf cufflinks_output_n1/transcripts.gtf cufflinks_output_n2/transcripts.gtf cufflinks_output_n3/transcripts.gtf
+cuffcompare -o cuffcompare_wt_out Danio_rerio.GRCz11.98.gtf  cufflinks_output_wt1/transcripts.gtf cufflinks_output_wt2/transcripts.gtf cufflinks_output_wt3/transcripts.gtf
+
+cuffmerge -g Danio_rerio.GRCz11.98.gtf cuffmerge.txt
+
+cuffdiff -o cuffdiff -L nsd1MZKO,WT cuffcompare_all_out.combined.gtf \
+n1_alignment.sam,n2_alignment.sam,n3_alignment.sam \
+wt1_alignment.sam,wt2_alignment.sam,wt3_alignment.sam
+
+
+cuffdiff -o differential_cufflinks_output/ n1_alignment.sorted.sam,n2_alignment.sorted.sam,n3_alignment.sorted.sam wt1_alignment.sorted.sam,wt2_alignment.sorted.sam,wt3_alignment.sorted.sam
