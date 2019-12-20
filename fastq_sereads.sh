82c6cde7d096405cc4551a76edf8328bf3f8d026
Audrey <audrey.calvird@uga.edu>
2019-12-20 15:31:37 -0500

    made things

--- fastq_sereads.sh
+++ fastq_sereads.sh
@@ -11,13 +11,8 @@ cd $PBS_O_WORKDIR
 cd /scratch/aec16885/morc3/
 
 module load FastQC/0.11.8-Java-1.8.0_144
-fastp -i rawfiles/s1.fastq.gz -o trimmedfiles/s1_trimmed.fq.gz --failed_out s1_failed.fq.gz -h s1
-fastp -i rawfiles/s2.fastq.gz -o trimmedfiles/s2_trimmed.fq.gz --failed_out s2_failed.fq.gz -h s2
-fastp -i rawfiles/s3.fastq.gz -o trimmedfiles/s3_trimmed.fq.gz --failed_out s3_failed.fq.gz -h s3
-fastp -i rawfiles/s4.fastq.gz -o trimmedfiles/s4_trimmed.fq.gz --failed_out s4_failed.fq.gz -h s4
-fastp -i rawfiles/s5.fastq.gz -o trimmedfiles/s5_trimmed.fq.gz --failed_out s5_failed.fq.gz -h s5
-fastp -i rawfiles/s6.fastq.gz -o trimmedfiles/s6_trimmed.fq.gz --failed_out s6_failed.fq.gz -h s6
+fastqc -o before_trim_fastqc s1.fastq.gz s2.fastq.gz s3.fastq.gz s4.fastq.gz s5.fastq.gz s6.fastq.gz
 
 module load MultiQC/1.5-foss-2016b-Python-2.7.14
 
-multiqc fastqc_trim_dir
+multiqc before_trim_fastqc
---
+++ seqtk_sereads.sh
@@ -0,0 +1,21 @@
+#PBS -S /bin/bash
+#PBS -q batch
+#PBS -N seqtk
+#PBS -l nodes=2:ppn=48:AMD
+#PBS -l walltime=72:00:00
+#PBS -l mem=10gb
+#PBS -M audrey.calvird@uga.edu
+#PBS -m abe
+
+cd $PBS_O_WORKDIR
+
+cd /scratch/aec16885/nsd1_RNAseq/bowtie
+
+module load seqtk/1.2-foss-2016b
+
+seqtk trimfq rawfiles/s1.fastq.gz > trimmedfiles/s1_trimmed.fq.gz
+seqtk trimfq rawfiles/s2.fastq.gz > trimmedfiles/s2_trimmed.fq.gz
+seqtk trimfq rawfiles/s3.fastq.gz > trimmedfiles/s3_trimmed.fq.gz
+seqtk trimfq rawfiles/s4.fastq.gz > trimmedfiles/s4_trimmed.fq.gz
+seqtk trimfq rawfiles/s5.fastq.gz > trimmedfiles/s5_trimmed.fq.gz
+seqtk trimfq rawfiles/s6.fastq.gz > trimmedfiles/s6_trimmed.fq.gz
