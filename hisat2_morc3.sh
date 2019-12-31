9079a449d7a294ec73e223a69491a62d1cd4a836
Audrey <audrey.calvird@uga.edu>
2019-12-31 16:25:56 -0500

    Create hisat2_morc3.sh

---
+++ hisat2_morc3.sh
@@ -0,0 +1,23 @@
+#PBS -S /bin/bash
+#PBS -q batch
+#PBS -N cufflinks
+#PBS -l nodes=1:ppn=48:AMD
+#PBS -l walltime=72:00:00
+#PBS -l mem=10gb
+#PBS -M audrey.calvird@uga.edu
+#PBS -m abe
+
+cd $PBS_O_WORKDIR
+cd /scratch/aec16885/morc3
+
+module load HISAT2/2.1.0-foss-2016b
+#build a histat2 index
+hisat2-build rawfiles/GCF_000002035.6_GRCz11_genomic.fna GRCz11_index
+
+#align trimmed reads
+hisat2 -q -x GRCz11_index -U trimmedfiles/s1_trimmed.fq.gz -S s1_alignment.sam
+hisat2 -q -x GRCz11_index -U trimmedfiles/s2_trimmed.fq.gz -S s2_alignment.sam
+hisat2 -q -x GRCz11_index -U trimmedfiles/s3_trimmed.fq.gz -S s3_alignment.sam
+hisat2 -q -x GRCz11_index -U trimmedfiles/s4_trimmed.fq.gz -S s4_alignment.sam
+hisat2 -q -x GRCz11_index -U trimmedfiles/s5_trimmed.fq.gz -S s5_alignment.sam
+hisat2 -q -x GRCz11_index -U trimmedfiles/s6_trimmed.fq.gz -S s6_alignment.sam
