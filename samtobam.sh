900cc7c22bac3699b08da6f2bb2f619c42953dbc
Audrey <audrey.calvird@uga.edu>
2020-02-24 13:14:27 -0500

    Update samtobam.sh

--- samtobam.sh
+++ samtobam.sh
@@ -1,27 +1,27 @@
 #PBS -S /bin/bash
 #PBS -q batch
 #PBS -N samtobam
-#PBS -l nodes=1:ppn=48:AMD
-#PBS -l walltime=10:00:00
+#PBS -l nodes=1:ppn=12
+#PBS -l walltime=2:00:00
 #PBS -l mem=8gb
 #PBS -M audrey.calvird@uga.edu
 #PBS -m abe
 cd $PBS_O_WORKDIR
 #change to directry where files are
-cd /scratch/aec16885/morc3
+cd /scratch/aec16885/nsd1
 
 module load SAMtools/1.6-foss-2016b
-scp s1_alignment.sam morc3bdel_1.sam
-scp s2_alignment.sam morc3bdel_2.sam
-scp s3_alignment.sam morc3bdel_3.sam
-scp s4_alignment.sam ab_1.sam
-scp s5_alignment.sam ab_2.sam
-scp s6_alignment.sam ab_3.sam
+scp n1_alignment.sam n1.sam
+scp n2_alignment.sam n2.sam
+scp n3_alignment.sam n3.sam
+scp wt1_alignment.sam ab_1.sam
+scp wt2_alignment.sam ab_2.sam
+scp wt3_alignment.sam ab_3.sam
 
 
-samtools view -b morc3bdel_1.sam > morc3bdel_1.bam
-samtools view -b morc3bdel_2.sam > morc3bdel_2.bam
-samtools view -b morc3bdel_3.sam > morc3bdel_3.bam
-samtools view -b ab_1.sam > ab_1.bam
-samtools view -b ab_2.sam > ab_2.bam
-samtools view -b ab_3.sam > ab_3.bam
+samtools view -S -b n1_alignment.sam > n1.bam
+samtools view -S -b n2_alignment.sam > n2.bam
+samtools view -S -b n3_alignment.sam > n2.bam
+samtools view -S -b wt1_alignment.sam > ab_1.bam
+samtools view -S -b wt2_alignment.sam > ab_2.bam
+samtools view -S -b wt3_alignment.sam > ab_3.bam
