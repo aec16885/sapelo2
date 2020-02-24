396f0a23c253a272680f41597e6363055e526d56
Audrey <audrey.calvird@uga.edu>
2020-02-24 15:34:42 -0500

    Update featureCounts_nsd1.sh

--- featureCounts_nsd1.sh
+++ featureCounts_nsd1.sh
@@ -12,4 +12,4 @@ cd /scratch/aec16885/nsd1
 
 module load Subread/1.6.2
 
-featureCounts -p -B -a GCF_000002035.6_GRCz11_genomic.gtf.gz -o nsd1_allignment_SAT1.txt n1_alignment.sam n2_alignment.sam n3_alignment.sam wt1_alignment.sam wt2_alignment.sam wt3_alignment.sam
+featureCounts -p -B -M -a GCF_000002035.6_GRCz11_genomic.gtf.gz -o nsd1_allignment_SAT1.txt n1_alignment.sam n2_alignment.sam n3_alignment.sam wt1_alignment.sam wt2_alignment.sam wt3_alignment.sam
