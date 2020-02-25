ae15f5697d1352200281d0ae0102c95c5e8f92e9
Audrey <audrey.calvird@uga.edu>
2020-02-25 14:49:38 -0500

    Update samtobam.sh

--- samtobam.sh
+++ samtobam.sh
@@ -11,17 +11,31 @@ cd $PBS_O_WORKDIR
 cd /scratch/aec16885/nsd1
 
 module load SAMtools/1.6-foss-2016b
-scp n1_alignment.sam n1.sam
-scp n2_alignment.sam n2.sam
-scp n3_alignment.sam n3.sam
-scp wt1_alignment.sam ab_1.sam
-scp wt2_alignment.sam ab_2.sam
-scp wt3_alignment.sam ab_3.sam
+#scp n1_alignment.sam n1.sam
+#scp n2_alignment.sam n2.sam
+#scp n3_alignment.sam n3.sam
+#scp wt1_alignment.sam ab_1.sam
+#scp wt2_alignment.sam ab_2.sam
+#scp wt3_alignment.sam ab_3.sam
 
 
-samtools view -S -b n1_alignment.sam > n1.bam
-samtools view -S -b n2_alignment.sam > n2.bam
-samtools view -S -b n3_alignment.sam > n2.bam
-samtools view -S -b wt1_alignment.sam > ab_1.bam
-samtools view -S -b wt2_alignment.sam > ab_2.bam
-samtools view -S -b wt3_alignment.sam > ab_3.bam
+#samtools view -S -b n1_alignment.sam > n1.bam
+#samtools view -S -b n2_alignment.sam > n2.bam
+#samtools view -S -b n3_alignment.sam > n3.bam
+#samtools view -S -b wt1_alignment.sam > ab_1.bam
+#samtools view -S -b wt2_alignment.sam > ab_2.bam
+#samtools view -S -b wt3_alignment.sam > ab_3.bam
+
+samtools sort n1.bam -o n1sorted.bam
+samtools sort n2.bam -o n2sorted.bam
+samtools sort n3.bam -o n3sorted.bam
+samtools sort ab_1.bam -o ab1sorted.bam
+samtools sort ab_2.bam -o ab1sorted.bam
+samtools sort ab_3.bam -o ab1sorted.bam
+
+samtools index n1sorted.bam
+samtools index n2sorted.bam
+samtools index n3sorted.bam
+samtools index ab1sorted.bam
+samtools index ab2sorted.bam
+samtools index ab3sorted.bam 
