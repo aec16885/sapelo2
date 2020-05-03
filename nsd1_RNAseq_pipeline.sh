2940c23f16ef0c581120bc675a32406809927d1e
Audrey <audrey.calvird@uga.edu>
2020-05-03 15:18:42 -0400

    Update nsd1_RNAseq_pipeline.sh

--- nsd1_RNAseq_pipeline.sh
+++ nsd1_RNAseq_pipeline.sh
@@ -29,15 +29,15 @@ cd /scratch/aec16885/nsd1/traditional_alignment
 #multiqc fastqc_analysis
 
 #align trimmed reads
-#module load HISAT2/2.1.0-foss-2016b
-#hisat2-build GCF_000002035.6_GRCz11_genomic.fna GRCz11_index
+module load HISAT2/2.1.0-foss-2016b
+hisat2-build danRer11.fa GRCz11_index
 
-#hisat2 -q --no-mixed --no-discordant -x GRCz11_index -1 Nsd1a_bdouble-1_IGO_07846_22_S31_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-1_IGO_07846_22_S31_R2_001_val_2.fq.gz -S n1_alignment.sam
-#hisat2 -q --no-mixed --no-discordant -x GRCz11_index -1 Nsd1a_bdouble-2_IGO_07846_23_S32_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-2_IGO_07846_23_S32_R2_001_val_2.fq.gz -S n2_alignment.sam
-#hisat2 -q --no-mixed --no-discordant -x GRCz11_index -1 Nsd1a_bdouble-3_IGO_07846_24_S33_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-3_IGO_07846_24_S33_R2_001_val_2.fq.gz -S n3_alignment.sam
-#hisat2 -q --no-mixed --no-discordant -x GRCz11_index -1 Wt5dpf-1_IGO_07846_19_S28_R1_001_val_1.fq.gz -2 Wt5dpf-1_IGO_07846_19_S28_R2_001_val_2.fq.gz -S wt1_alignment.sam
-#hisat2 -q --no-mixed --no-discordant -x GRCz11_index -1 Wt5ddf-2_IGO_07846_20_S29_R1_001_val_1.fq.gz -2 Wt5ddf-2_IGO_07846_20_S29_R2_001_val_2.fq.gz -S wt2_alignment.sam
-#hisat2 -q --no-mixed --no-discordant -x GRCz11_index -1 Wt5ddf-3_IGO_07846_21_S30_R1_001_val_1.fq.gz -2 Wt5ddf-3_IGO_07846_21_S30_R2_001_val_2.fq.gz -S wt3_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Nsd1a_bdouble-1_IGO_07846_22_S31_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-1_IGO_07846_22_S31_R2_001_val_2.fq.gz -S n1_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Nsd1a_bdouble-2_IGO_07846_23_S32_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-2_IGO_07846_23_S32_R2_001_val_2.fq.gz -S n2_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Nsd1a_bdouble-3_IGO_07846_24_S33_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-3_IGO_07846_24_S33_R2_001_val_2.fq.gz -S n3_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 GRCz11_index -1 Wt5dpf-1_IGO_07846_19_S28_R1_001_val_1.fq.gz -2 Wt5dpf-1_IGO_07846_19_S28_R2_001_val_2.fq.gz -S wt1_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 GRCz11_index -1 Wt5ddf-2_IGO_07846_20_S29_R1_001_val_1.fq.gz -2 Wt5ddf-2_IGO_07846_20_S29_R2_001_val_2.fq.gz -S wt2_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 GRCz11_index -1 Wt5ddf-3_IGO_07846_21_S30_R1_001_val_1.fq.gz -2 Wt5ddf-3_IGO_07846_21_S30_R2_001_val_2.fq.gz -S wt3_alignment.sam
 
 #count aligned reads
 #module load HTSeq/0.9.1-foss-2016b-Python-2.7.14
