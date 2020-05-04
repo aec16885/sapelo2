fa2e79a7eefad257dbf04728ca3511ac3d0092d4
Audrey <audrey.calvird@uga.edu>
2020-05-04 08:38:53 -0400

    repeat aligning

--- nsd1_sat1RNAseq_pipeline.sh
+++ nsd1_sat1RNAseq_pipeline.sh
@@ -8,7 +8,7 @@
 #PBS -m abe
 
 cd $PBS_O_WORKDIR
-cd /scratch/aec16885/nsd1/traditional_alignment
+cd /scratch/aec16885/nsd1/sat1_alignment
 
 #trim reads
 #module load Trim_Galore/0.6.5-GCCcore-8.2.0-Java-11
@@ -30,20 +30,20 @@ cd /scratch/aec16885/nsd1/traditional_alignment
 
 #align trimmed reads
 module load HISAT2/2.1.0-foss-2016b
-hisat2-build danRer11.fa GRCz11_index
+hisat2-build main_sat1_sequence_1992.fasta sat1_index
 
-hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Nsd1a_bdouble-1_IGO_07846_22_S31_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-1_IGO_07846_22_S31_R2_001_val_2.fq.gz -S n1_alignment.sam
-hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Nsd1a_bdouble-2_IGO_07846_23_S32_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-2_IGO_07846_23_S32_R2_001_val_2.fq.gz -S n2_alignment.sam
-hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Nsd1a_bdouble-3_IGO_07846_24_S33_R1_001_val_1.fq.gz -2 Nsd1a_bdouble-3_IGO_07846_24_S33_R2_001_val_2.fq.gz -S n3_alignment.sam
-hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Wt5dpf-1_IGO_07846_19_S28_R1_001_val_1.fq.gz -2 Wt5dpf-1_IGO_07846_19_S28_R2_001_val_2.fq.gz -S wt1_alignment.sam
-hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Wt5ddf-2_IGO_07846_20_S29_R1_001_val_1.fq.gz -2 Wt5ddf-2_IGO_07846_20_S29_R2_001_val_2.fq.gz -S wt2_alignment.sam
-hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x GRCz11_index -1 Wt5ddf-3_IGO_07846_21_S30_R1_001_val_1.fq.gz -2 Wt5ddf-3_IGO_07846_21_S30_R2_001_val_2.fq.gz -S wt3_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x sat1_index -1 ../traditional_alignment/Nsd1a_bdouble-1_IGO_07846_22_S31_R1_001_val_1.fq.gz -2 ../traditional_alignment/Nsd1a_bdouble-1_IGO_07846_22_S31_R2_001_val_2.fq.gz -S n1_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x sat1_index -1 ../traditional_alignment/Nsd1a_bdouble-2_IGO_07846_23_S32_R1_001_val_1.fq.gz -2 ../traditional_alignment/Nsd1a_bdouble-2_IGO_07846_23_S32_R2_001_val_2.fq.gz -S n2_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x sat1_index -1 ../traditional_alignment/Nsd1a_bdouble-3_IGO_07846_24_S33_R1_001_val_1.fq.gz -2 ../traditional_alignment/Nsd1a_bdouble-3_IGO_07846_24_S33_R2_001_val_2.fq.gz -S n3_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x sat1_index -1 ../traditional_alignment/Wt5dpf-1_IGO_07846_19_S28_R1_001_val_1.fq.gz -2 ../traditional_alignment/Wt5dpf-1_IGO_07846_19_S28_R2_001_val_2.fq.gz -S wt1_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x sat1_index -1 ../traditional_alignment/Wt5ddf-2_IGO_07846_20_S29_R1_001_val_1.fq.gz -2 ../traditional_alignment/Wt5ddf-2_IGO_07846_20_S29_R2_001_val_2.fq.gz -S wt2_alignment.sam
+hisat2 -q --no-mixed --rna-strandness FR --secondary -k 10 -x sat1_index -1 ../traditional_alignment/Wt5ddf-3_IGO_07846_21_S30_R1_001_val_1.fq.gz -2 ../traditional_alignment/Wt5ddf-3_IGO_07846_21_S30_R2_001_val_2.fq.gz -S wt3_alignment.sam
 
 #count aligned reads
 #module load HTSeq/0.9.1-foss-2016b-Python-2.7.14
 
 #htseq-count -r name -s yes -a 10 -t exon -i gene_id -m intersection-nonempty --nonunique=all --secondary-alignments=ignore n1_alignment.sam n2_alignment.sam n3_alignment.sam wt1_alignment.sam wt2_alignment.sam wt3_alignment.sam danRer11.repeatmsk.gtf.gz > nsd1_repeats_countsfile.txt
 
-module load Subread/1.6.2
+#module load Subread/1.6.2
 
-featureCounts -p -B -M -O --fraction -g transcript_id -a danRer11.repeatmsk.gtf.gz -o repeat_alignment.txt n1_alignment.sam n2_alignment.sam n3_alignment.sam wt1_alignment.sam wt2_alignment.sam wt3_alignment.sam
+#featureCounts -p -B -M -O --fraction -g transcript_id -a danRer11.repeatmsk.gtf.gz -o repeat_alignment.txt n1_alignment.sam n2_alignment.sam n3_alignment.sam wt1_alignment.sam wt2_alignment.sam wt3_alignment.sam
