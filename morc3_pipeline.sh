5211bcd8a39ad6036733410435471f24f518ac00
Audrey <audrey.calvird@uga.edu>
2020-05-21 11:44:19 -0400

    Update morc3_pipeline.sh

--- morc3_pipeline.sh
+++ morc3_pipeline.sh
@@ -30,22 +30,22 @@ cd /scratch/aec16885/morc3/RNA_seq/
 #gunzip danRer11.fa.gz
 #gunzip danRer11.ncbiRefSeq.gtf.gz
 
-module load STAR/2.7.1a-foss-2016b
+#module load STAR/2.7.1a-foss-2016b
 
 #STAR --runThreadN 48 --runMode genomeGenerate --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --genomeFastaFiles /scratch/aec16885/morc3/RNA_seq/danRer11.fa --sjdbGTFfile /scratch/aec16885/morc3/RNA_seq/danRer11.ncbiRefSeq.gtf --sjdbOverhang 100
-gunzip *.fq.gz
+#gunzip *.fq.gz
 
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-1_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-1_R2_001_val_2.fq --outFileNamePrefix morc3ab_6hpf_1_ --outSAMtype BAM SortedByCoordinate
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-2_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-2_R2_001_val_2.fq --outFileNamePrefix morc3ab_6hpf_2_ --outSAMtype BAM SortedByCoordinate
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-3_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-3_R2_001_val_2.fq --outFileNamePrefix morc3ab_6hpf_3_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-1_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-1_R2_001_val_2.fq --outFileNamePrefix morc3ab_6hpf_1_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-2_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-2_R2_001_val_2.fq --outFileNamePrefix morc3ab_6hpf_2_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-3_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3ab-6hpf-3_R2_001_val_2.fq --outFileNamePrefix morc3ab_6hpf_3_ --outSAMtype BAM SortedByCoordinate
 
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-1_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-1_R2_001_val_2.fq --outFileNamePrefix morc3b_6hpf_1_ --outSAMtype BAM SortedByCoordinate
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-2_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-2_R2_001_val_2.fq --outFileNamePrefix morc3b_6hpf_2_ --outSAMtype BAM SortedByCoordinate
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-3_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-3_R2_001_val_2.fq --outFileNamePrefix morc3b_6hpf_3_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-1_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-1_R2_001_val_2.fq --outFileNamePrefix morc3b_6hpf_1_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-2_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-2_R2_001_val_2.fq --outFileNamePrefix morc3b_6hpf_2_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-3_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/morc3b-6hpf-3_R2_001_val_2.fq --outFileNamePrefix morc3b_6hpf_3_ --outSAMtype BAM SortedByCoordinate
 
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/wt-6hpf-1_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/wt-6hpf-1_R2_001_val_2.fq --outFileNamePrefix wt_6hpf_1_ --outSAMtype BAM SortedByCoordinate
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/wt-6hpf-2_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/wt-6hpf-2_R2_001_val_2.fq --outFileNamePrefix wt_6hpf_2_ --outSAMtype BAM SortedByCoordinate
-STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/wt-6hpf-3_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/wt-6hpf-3_R2_001_val_2.fq --outFileNamePrefix wt_6hpf_3_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/wt-6hpf-1_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/wt-6hpf-1_R2_001_val_2.fq --outFileNamePrefix wt_6hpf_1_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/wt-6hpf-2_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/wt-6hpf-2_R2_001_val_2.fq --outFileNamePrefix wt_6hpf_2_ --outSAMtype BAM SortedByCoordinate
+#STAR --runThreadN 48 --genomeDir /scratch/aec16885/morc3/RNA_seq/danRer11_index --readFilesIn /scratch/aec16885/morc3/RNA_seq/wt-6hpf-3_R1_001_val_1.fq /scratch/aec16885/morc3/RNA_seq/wt-6hpf-3_R2_001_val_2.fq --outFileNamePrefix wt_6hpf_3_ --outSAMtype BAM SortedByCoordinate
 
 module load Subread/1.6.2
-featureCounts -p -B -O -g gene_id -a Danio_rerio.GRCz11.98.gtf -o morc3_6hpf_counts.txt morc3ab_6hpf_1_Aligned.sortedByCoord.out.bam morc3ab_6hpf_2_Aligned.sortedByCoord.out.bam morc3ab_6hpf_3_Aligned.sortedByCoord.out.bam morc3b_6hpf_1_Aligned.sortedByCoord.out.bam morc3b_6hpf_2_Aligned.sortedByCoord.out.bam morc3b_6hpf_3_Aligned.sortedByCoord.out.bam wt_6hpf_1_Aligned.sortedByCoord.out.bam wt_6hpf_2_Aligned.sortedByCoord.out.bam wt_6hpf_3_Aligned.sortedByCoord.out.bam
+featureCounts -p -B -O -g gene_id -a danRer11.ncbiRefSeq.gtf -o morc3_6hpf_counts.txt morc3ab_6hpf_1_Aligned.sortedByCoord.out.bam morc3ab_6hpf_2_Aligned.sortedByCoord.out.bam morc3ab_6hpf_3_Aligned.sortedByCoord.out.bam morc3b_6hpf_1_Aligned.sortedByCoord.out.bam morc3b_6hpf_2_Aligned.sortedByCoord.out.bam morc3b_6hpf_3_Aligned.sortedByCoord.out.bam wt_6hpf_1_Aligned.sortedByCoord.out.bam wt_6hpf_2_Aligned.sortedByCoord.out.bam wt_6hpf_3_Aligned.sortedByCoord.out.bam
