9ceefea99b414a611aff013d7c4dd083fa6c31c8
Audrey <audrey.calvird@uga.edu>
2020-05-29 13:37:07 -0400

    Update bigwig_from_bam.sh

--- bigwig_from_bam.sh
+++ bigwig_from_bam.sh
@@ -12,28 +12,28 @@ cd /scratch/aec16885/morc3/RNA_seq/
 
 module load SAMtools/1.10-GCC-8.2.0-2.31.1
 
-samtools index morc3ab_6hpf_1_Aligned.sortedByCoord.out.bam
-samtools index morc3ab_6hpf_2_Aligned.sortedByCoord.out.bam
-samtools index morc3ab_6hpf_3_Aligned.sortedByCoord.out.bam
+samtools index morc3ab_24hpf_1_Aligned.sortedByCoord.out.bam
+samtools index morc3ab_24hpf_2_Aligned.sortedByCoord.out.bam
+samtools index morc3ab_24hpf_3_Aligned.sortedByCoord.out.bam
 
-samtools index morc3b_6hpf_1_Aligned.sortedByCoord.out.bam
-samtools index morc3b_6hpf_2_Aligned.sortedByCoord.out.bam
-samtools index morc3b_6hpf_3_Aligned.sortedByCoord.out.bam
+samtools index morc3b_24hpf_1_Aligned.sortedByCoord.out.bam
+samtools index morc3b_24hpf_2_Aligned.sortedByCoord.out.bam
+samtools index morc3b_24hpf_3_Aligned.sortedByCoord.out.bam
 
-samtools index wt_6hpf_1_Aligned.sortedByCoord.out.bam
-samtools index wt_6hpf_2_Aligned.sortedByCoord.out.bam
-samtools index wt_6hpf_3_Aligned.sortedByCoord.out.bam
+samtools index wt_24hpf_1_Aligned.sortedByCoord.out.bam
+samtools index wt_24hpf_2_Aligned.sortedByCoord.out.bam
+samtools index wt_24hpf_3_Aligned.sortedByCoord.out.bam
 
 module load deepTools/3.3.1-intel-2019b-Python-3.7.4
 
-bamCoverage -bs 10 -p max -b morc3ab_6hpf_1_Aligned.sortedByCoord.out.bam -o morc3ab_6hpf_1.bw
-bamCoverage -bs 10 -p max -b morc3ab_6hpf_2_Aligned.sortedByCoord.out.bam -o morc3ab_6hpf_2.bw
-bamCoverage -bs 10 -p max -b morc3ab_6hpf_3_Aligned.sortedByCoord.out.bam -o morc3ab_6hpf_3.bw
+bamCoverage -bs 10 -p max -b morc3ab_24hpf_1_Aligned.sortedByCoord.out.bam -o morc3ab_24hpf_1.bw
+bamCoverage -bs 10 -p max -b morc3ab_24hpf_2_Aligned.sortedByCoord.out.bam -o morc3ab_24hpf_2.bw
+bamCoverage -bs 10 -p max -b morc3ab_24hpf_3_Aligned.sortedByCoord.out.bam -o morc3ab_24hpf_3.bw
 
-bamCoverage -bs 10 -p max -b morc3b_6hpf_1_Aligned.sortedByCoord.out.bam -o morc3b_6hpf_1.bw
-bamCoverage -bs 10 -p max -b morc3b_6hpf_2_Aligned.sortedByCoord.out.bam -o morc3b_6hpf_2.bw
-bamCoverage -bs 10 -p max -b morc3b_6hpf_3_Aligned.sortedByCoord.out.bam -o morc3b_6hpf_3.bw
+bamCoverage -bs 10 -p max -b morc3b_24hpf_1_Aligned.sortedByCoord.out.bam -o morc3b_24hpf_1.bw
+bamCoverage -bs 10 -p max -b morc3b_24hpf_2_Aligned.sortedByCoord.out.bam -o morc3b_24hpf_2.bw
+bamCoverage -bs 10 -p max -b morc3b_24hpf_3_Aligned.sortedByCoord.out.bam -o morc3b_24hpf_3.bw
 
-bamCoverage -bs 10 -p max -b wt_6hpf_1_Aligned.sortedByCoord.out.bam -o wt_6hpf_1.bw
-bamCoverage -bs 10 -p max -b wt_6hpf_2_Aligned.sortedByCoord.out.bam -o wt_6hpf_2.bw
-bamCoverage -bs 10 -p max -b wt_6hpf_3_Aligned.sortedByCoord.out.bam -o wt_6hpf_3.bw
+bamCoverage -bs 10 -p max -b wt_24hpf_1_Aligned.sortedByCoord.out.bam -o wt_24hpf_1.bw
+bamCoverage -bs 10 -p max -b wt_24hpf_2_Aligned.sortedByCoord.out.bam -o wt_24hpf_2.bw
+bamCoverage -bs 10 -p max -b wt_24hpf_3_Aligned.sortedByCoord.out.bam -o wt_24hpf_3.bw
