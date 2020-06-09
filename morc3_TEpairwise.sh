adfd4228ce709f00b262d63b9102533c8572c778
Audrey <audrey.calvird@uga.edu>
2020-06-09 10:46:08 -0400

    Update morc3_TEpairwise.sh

--- morc3_TEpairwise.sh
+++ morc3_TEpairwise.sh
@@ -11,9 +11,12 @@ cd $PBS_O_WORKDIR
 cd /scratch/aec16885/morc3/RNA_seq/
 
 module load Subread/1.6.2
-featureCounts -p -B -O -g gene_id -a danRer11.repeatmsk.gtf -o morc3ab_6vs24hpf_TEcounts.txt morc3ab_6hpf_1_Aligned.sortedByCoord.out.bam morc3ab_6hpf_2_Aligned.sortedByCoord.out.bam morc3ab_6hpf_3_Aligned.sortedByCoord.out.bam morc3ab_24hpf_1_Aligned.sortedByCoord.out.bam morc3ab_24hpf_2_Aligned.sortedByCoord.out.bam morc3ab_24hpf_3_Aligned.sortedByCoord.out.bam
+#featureCounts -p -B -O -g gene_id -a danRer11.repeatmsk.gtf -o morc3ab_6vs24hpf_TEcounts.txt morc3ab_6hpf_1_Aligned.sortedByCoord.out.bam morc3ab_6hpf_2_Aligned.sortedByCoord.out.bam morc3ab_6hpf_3_Aligned.sortedByCoord.out.bam morc3ab_24hpf_1_Aligned.sortedByCoord.out.bam morc3ab_24hpf_2_Aligned.sortedByCoord.out.bam morc3ab_24hpf_3_Aligned.sortedByCoord.out.bam
 
-featureCounts -p -B -O -g gene_id -a danRer11.repeatmsk.gtf -o morc3b_6vs24hpf_TEcounts.txt morc3b_6hpf_1_Aligned.sortedByCoord.out.bam morc3b_6hpf_2_Aligned.sortedByCoord.out.bam morc3b_6hpf_3_Aligned.sortedByCoord.out.bam morc3b_24hpf_1_Aligned.sortedByCoord.out.bam morc3b_24hpf_2_Aligned.sortedByCoord.out.bam morc3b_24hpf_3_Aligned.sortedByCoord.out.bam
+#featureCounts -p -B -O -g gene_id -a danRer11.repeatmsk.gtf -o morc3b_6vs24hpf_TEcounts.txt morc3b_6hpf_1_Aligned.sortedByCoord.out.bam morc3b_6hpf_2_Aligned.sortedByCoord.out.bam morc3b_6hpf_3_Aligned.sortedByCoord.out.bam morc3b_24hpf_1_Aligned.sortedByCoord.out.bam morc3b_24hpf_2_Aligned.sortedByCoord.out.bam morc3b_24hpf_3_Aligned.sortedByCoord.out.bam
+
+featureCounts -p -B -O -g gene_id -a danRer11.repeatmsk.gtf -o wt_6vs24hpf_TEcounts.txt wt_6hpf_1_Aligned.sortedByCoord.out.bam wt_6hpf_2_Aligned.sortedByCoord.out.bam wt_6hpf_3_Aligned.sortedByCoord.out.bam wt_24hpf_1_Aligned.sortedByCoord.out.bam wt_24hpf_2_Aligned.sortedByCoord.out.bam wt_24hpf_3_Aligned.sortedByCoord.out.bam
+
 
 #featureCounts -p -B -O -g gene_id -a danRer11.repeatmsk.gtf -o morc3abvsWT_6hpf_TEcounts.txt morc3ab_6hpf_1_Aligned.sortedByCoord.out.bam morc3ab_6hpf_2_Aligned.sortedByCoord.out.bam morc3ab_6hpf_3_Aligned.sortedByCoord.out.bam wt_6hpf_1_Aligned.sortedByCoord.out.bam wt_6hpf_2_Aligned.sortedByCoord.out.bam wt_6hpf_3_Aligned.sortedByCoord.out.bam
 
