cc30065f293600616a6ece865fa21b7a59476600
Audrey <audrey.calvird@uga.edu>
2022-04-21 10:55:33 -0400

    Update nanog_fitcutandrun.sh
    added trimming and bowtie2

--- nanog_fitcutandrun.sh
+++ nanog_fitcutandrun.sh
@@ -11,11 +11,19 @@
 #SBATCH --mail-user=aec16885@uga.edu  # Where to send mail
 
 cd $SLURM_SUBMIT_DIR
-cd /scratch/aec16885/morc3/
+cd /scratch/aec16885/nanog/fitCUTandRUN
 
-module load FastQC/0.11.8-Java-1.8.0_144
-fastqc -o before_trim_fastqc s1.fastq.gz s2.fastq.gz s3.fastq.gz s4.fastq.gz s5.fastq.gz s6.fastq.gz
+#trim reads
+module load Trim_Galore/0.6.5-GCCcore-8.2.0-Java-11
 
-module load MultiQC/1.5-foss-2016b-Python-2.7.14
+trim_galore -q 20 --paired replicate1/CRR278533_f1.fastq.gz  replicate1/CRR278533_r2.fastq.gz
+trim_galore -q 20 --paired replicate2/CRR278534_f1.fastq.gz  replicate2/CRR278534_r2.fastq.gz
+trim_galore -q 20 --paired replicate3/CRR278535_f1.fastq.gz  replicate3/CRR278535_r2.fastq.gz
 
-multiqc before_trim_fastqc
+module load ml Bowtie2/2.4.1-GCC-8.3.0
+
+bowtie2-build -f work/mglab/anh/reference_genome/main_sat1_sequence_1992.fasta sat1_index
+
+bowtie2 -x sat1_index -1 CRR278533_f1* -2 CRR278533_r2* -U sample1_unpaired --interleaved sample1_interleaved -b sample1 -S sample1_aligned
+bowtie2 -x sat1_index -1 CRR278534_f1* -2 CRR278534_r2* -U sample2_unpaired --interleaved sample2_interleaved -b sample2 -S sample2_aligned
+bowtie2 -x sat1_index -1 CRR278535_f1* -2 CRR278535_r2* -U sample3_unpaired --interleaved sample3_interleaved -b sample3 -S sample3_aligned
