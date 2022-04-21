7752b20ab8a41b6f6781a93e7b9a285733877d6e
Audrey <audrey.calvird@uga.edu>
2022-04-21 14:47:16 -0400

    Update nanog_fitcutandrun.sh
    update modules

--- nanog_fitcutandrun.sh
+++ nanog_fitcutandrun.sh
@@ -7,20 +7,20 @@
 #SBATCH --output=nanog_cutandrun_trial1.%j.out    # Standard output log
 #SBATCH --error=nanog_cutandrun_trial1.%j.err     # Standard error log
 
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=aec16885@uga.edu  # Where to send mail
 
 cd $SLURM_SUBMIT_DIR
 cd /scratch/aec16885/nanog/fitCUTandRUN
 
 #trim reads
-module load Trim_Galore/0.6.5-GCCcore-8.2.0-Java-11
+module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
 
 trim_galore -q 20 --paired replicate1/CRR278533_f1.fastq.gz  replicate1/CRR278533_r2.fastq.gz
 trim_galore -q 20 --paired replicate2/CRR278534_f1.fastq.gz  replicate2/CRR278534_r2.fastq.gz
 trim_galore -q 20 --paired replicate3/CRR278535_f1.fastq.gz  replicate3/CRR278535_r2.fastq.gz
 
-module load ml Bowtie2/2.4.1-GCC-8.3.0
+module load Bowtie2/2.4.5-GCC-10.2.0
 
 bowtie2-build -f work/mglab/anh/reference_genome/main_sat1_sequence_1992.fasta sat1_index
 
