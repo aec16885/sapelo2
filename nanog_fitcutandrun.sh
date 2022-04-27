3d526ce9d1e46df7857ed8fe32ab6dba07852715
Audrey <audrey.calvird@uga.edu>
2022-04-27 13:26:47 -0400

    Update nanog_fitcutandrun.sh
    took out interleaved command

--- nanog_fitcutandrun.sh
+++ nanog_fitcutandrun.sh
@@ -22,8 +22,8 @@ cd /scratch/aec16885/nanog/fitCUTandRUN
 
 module load Bowtie2/2.4.5-GCC-10.2.0
 
-bowtie2-build -f /work/mglab/anh/reference_genome/main_sat1_sequence_1992.fasta sat1_index
+#bowtie2-build -f /work/mglab/anh/reference_genome/main_sat1_sequence_1992.fasta sat1_index
 
-bowtie2 -x sat1_index -1 CRR278533_f1* -2 CRR278533_r2* -U sample1_unpaired --interleaved sample1_interleaved -b sample1 -S sample1_aligned
-bowtie2 -x sat1_index -1 CRR278534_f1* -2 CRR278534_r2* -U sample2_unpaired --interleaved sample2_interleaved -b sample2 -S sample2_aligned
-bowtie2 -x sat1_index -1 CRR278535_f1* -2 CRR278535_r2* -U sample3_unpaired --interleaved sample3_interleaved -b sample3 -S sample3_aligned
+bowtie2 -x sat1_index -1 CRR278533_f1* -2 CRR278533_r2* -U sample1_unpaired  -b sample1 -S sample1_aligned
+bowtie2 -x sat1_index -1 CRR278534_f1* -2 CRR278534_r2* -U sample2_unpaired  -b sample2 -S sample2_aligned
+bowtie2 -x sat1_index -1 CRR278535_f1* -2 CRR278535_r2* -U sample3_unpaired  -b sample3 -S sample3_aligned
